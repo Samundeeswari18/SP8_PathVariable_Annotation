c4570df3abfba9f2ceafe751d16d9d906a99ab38
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-06 15:00:24 -0400

    PathVariable Annotation

---
+++ src/main/java/com/sam/sp8_pathvariable_annotation/Sp8PathVariableAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.sam.sp8_pathvariable_annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp8PathVariableAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp8PathVariableAnnotationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/sp8_pathvariable_annotation/Sp8PathVariableAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.sp8_pathvariable_annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp8PathVariableAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
